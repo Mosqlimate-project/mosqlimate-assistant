ece2199669b998126563cea392b08f20dc7d0f6c
Sillas Rocha <sillasrocha29@gmail.com>
2025-02-19 08:30:42 -0300

    Added table filters to select the desired table from api

--- mosqlimate_assistant/filters.py
+++ mosqlimate_assistant/filters.py
@@ -1,6 +1,35 @@
 from typing import Optional, Literal
 from pydantic import BaseModel, Field, constr
 
+class TableFilters(BaseModel):
+    table: Literal['infodengue', 'climate', 'mosquito', 'episcanner'] = Field(
+        ..., description="Tabela a ser consultada: 'infodengue', 'climate', 'mosquito' ou 'episcanner'"
+    )
+    disease: Optional[Literal['dengue', 'zika', 'chik', 'chikungunya']] = Field(
+        None, description="Doença: 'dengue', 'zika', 'chik' ou 'chikungunya'"
+    )
+    start: Optional[constr(pattern=r'^\d{4}-\d{2}-\d{2}$')] = Field(
+        None, description="Data de início (YYYY-mm-dd)"
+    )
+    end: Optional[constr(pattern=r'^\d{4}-\d{2}-\d{2}$')] = Field(
+        None, description="Data de término (YYYY-mm-dd)"
+    )
+    uf: Optional[Literal["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]] = Field(
+        None, description="Sigla do estado brasileiro (duas letras), ex: SP"
+    )
+    geocode: Optional[int] = Field(
+        None, description="Código do município segundo o IBGE"
+    )
+    key: Optional[str] = Field(
+        None, description="ContaOvos API key"
+    )
+    year: Optional[int] = Field(
+        None, description="Ano específico"
+    )
+
+    class Config:
+        extra = "ignore"
+
 class InfodengueFilters(BaseModel):
     disease: Literal['dengue', 'zika', 'chik', 'chikungunya'] = Field(
         ..., description="Doença: 'dengue', 'zika', 'chik' ou 'chikungunya'"
