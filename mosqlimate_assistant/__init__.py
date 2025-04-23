72bb845e005486add58a924e5a74d20b22353ae0
Luã Bida Vacaro <luabidaa@gmail.com>
2025-04-23 14:15:52 -0300

    chore(package): implement semantic-release

--- mosqlimate_assistant/__init__.py
+++ mosqlimate_assistant/__init__.py
@@ -1,6 +1,21 @@
+from importlib import metadata as importlib_metadata
+from typing import List
+
 from .assistant import *
 from .configs import *
 from .filters import *
 from .input_validator import *
 from .prompts import eng, por
 from .utils import *
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.1.0"  # changed by semantic-release
+
+
+version: str = get_version()
+__version__: str = version
+__all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
