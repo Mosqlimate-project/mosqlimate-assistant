7852fc988e9ed56bb6a2c9836da67b2f1c2e523e
Sillas Rocha <sillasrocha29@gmail.com>
2025-07-01 15:31:36 -0300

    fix(tests): reorganize test imports and remove unused test_faiss_db.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,14 @@
-from . import test_faiss_db, test_muni_codes, test_schemas, test_settings
+from . import (
+    test_api_consumer,
+    test_docs_consumer,
+    test_muni_codes,
+    test_schemas,
+    test_settings,
+)
 
 __all__ = [
-    "test_faiss_db",
+    "test_api_consumer",
+    "test_docs_consumer",
     "test_muni_codes",
     "test_settings",
     "test_schemas",
