002d9c78152bb98dfb7a16d8c8cdd65d0f855677
Sillas Rocha <sillasrocha29@gmail.com>
2026-03-12 10:48:11 -0300

    feat(tests): add unit tests for settings

---
+++ tests/test_settings.py
@@ -0,0 +1,12 @@
+import os
+from mosqlimate_assistant.settings import (
+    HTTP_CACHE_ENABLED,
+    HTTP_CACHE_DIR,
+    HTTP_CACHE_TTL_SECONDS,
+)
+
+
+def test_settings_types():
+    assert isinstance(HTTP_CACHE_ENABLED, bool)
+    assert isinstance(HTTP_CACHE_DIR, str)
+    assert isinstance(HTTP_CACHE_TTL_SECONDS, int)
